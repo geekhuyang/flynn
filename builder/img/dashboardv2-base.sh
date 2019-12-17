77b4b80125db60d3b99fc8625718834f78b9ef62
Jesse Stuart <email@jessestuart.ca>
2019-12-17 15:16:50 -0500

    builder: Compile controller gRPC service for dashboardv2
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- builder/img/dashboardv2-base.sh
+++ builder/img/dashboardv2-base.sh
@@ -6,24 +6,6 @@ apt-get update
 apt-get install --yes unzip python make
 apt-get clean
 
-# install protobuf compiler
-pbversion="3.7.1"
-shasum="24ea6924faaf94d4a0c5850fdb278290a326eff9a68f36ee5809654faccd0e10"
-tmpdir=$(mktemp --directory)
-trap "rm -rf ${tmpdir}" EXIT
-curl -sL https://github.com/google/protobuf/releases/download/v${pbversion}/protoc-${pbversion}-linux-x86_64.zip > "${tmpdir}/protoc.zip"
-echo "${shasum}  ${tmpdir}/protoc.zip" | shasum -c -
-unzip -d "${tmpdir}/protoc" "${tmpdir}/protoc.zip"
-mv "${tmpdir}/protoc" /opt
-ln -s /opt/protoc/bin/protoc /usr/local/bin/protoc
-
-# install googleapis common protos
-shasum="9584b7ac21de5b31832faf827f898671cdcb034bd557a36ea3e7fc07e6571dcb"
-curl -fSLo /tmp/common-protos.tar.gz "https://github.com/googleapis/googleapis/archive/common-protos-1_3_1.tar.gz"
-echo "${shasum}  /tmp/common-protos.tar.gz" | shasum -c -
-tar xzf /tmp/common-protos.tar.gz -C "/opt/protoc/include" --strip-components=1
-rm /tmp/common-protos.tar.gz
-
 # install nodejs
 nodeversion="8.11.4"
 nodeshasum="c69abe770f002a7415bd00f7ea13b086650c1dd925ef0c3bf8de90eabecc8790"
@@ -36,10 +18,6 @@ rm /tmp/node.tar.gz
 nodebin="/usr/local/node-v${nodeversion}-linux-x64/bin"
 ln -nfs ${nodebin}/node /usr/local/bin/node
 ln -nfs ${nodebin}/npm /usr/local/bin/npm
-
-# install typescript protoc (https://github.com/improbable-eng/ts-protoc-gen)
-npm install -g google-protobuf@3.11.2 ts-protoc-gen@0.12.0
-ln -nfs ${nodebin}/protoc-gen-ts /usr/local/bin/protoc-gen-ts
 
 # install yarn
 npm install -g yarn@1.21.1
