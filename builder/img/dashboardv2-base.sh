69c5a9f38e6f5836b3f937cd00f35c3a87250a53
Jesse Stuart <email@jessestuart.ca>
2019-12-17 14:19:55 -0500

    builder: Fix dashboardv2-compiled
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- builder/img/dashboardv2-base.sh
+++ builder/img/dashboardv2-base.sh
@@ -27,19 +27,20 @@ rm /tmp/common-protos.tar.gz
 # install nodejs
 nodeversion="8.11.4"
 nodeshasum="c69abe770f002a7415bd00f7ea13b086650c1dd925ef0c3bf8de90eabecc8790"
-nodedir="/usr/local"
 curl -fSLo /tmp/node.tar.gz "https://nodejs.org/dist/v${nodeversion}/node-v${nodeversion}-linux-x64.tar.gz"
 echo "${nodeshasum}  /tmp/node.tar.gz" | shasum -c -
-tar xzf /tmp/node.tar.gz -C "${nodedir}"
+tar xzf /tmp/node.tar.gz -C "/usr/local"
 rm /tmp/node.tar.gz
 
 # link nodejs binary
-nodebin="${nodedir}/node-v${nodeversion}-linux-x64/bin"
-ln -nfs ${nodebin}/node ${nodedir}/bin/node
-ln -nfs ${nodebin}/npm ${nodedir}/bin/npm
+nodebin="/usr/local/node-v${nodeversion}-linux-x64/bin"
+ln -nfs ${nodebin}/node /usr/local/bin/node
+ln -nfs ${nodebin}/npm /usr/local/bin/npm
 
 # install typescript protoc (https://github.com/improbable-eng/ts-protoc-gen)
 npm install -g google-protobuf@3.11.2 ts-protoc-gen@0.12.0
+ln -nfs ${nodebin}/protoc-gen-ts /usr/local/bin/protoc-gen-ts
 
 # install yarn
 npm install -g yarn@1.21.1
+ln -nfs ${nodebin}/yarn /usr/local/bin/yarn
